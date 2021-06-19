c07af1762de9995f2bf12c2900db4226832a7049
Evaristo Alves <evaristocarvalho34@gmail.com>
2021-06-19 10:28:35 -0300

    criando repositorio

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,12 @@
+using System.Security.AccessControl;
+using System;
+using System.Collections.Generic;
+using Dio.Series.Inerfaces;
+
+namespace Dio.Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dio.Series.Classes
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
